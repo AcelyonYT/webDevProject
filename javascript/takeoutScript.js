f2eae6279d484366670ca855637214868a287215
AcelyonYT <95740136+AcelyonYT@users.noreply.github.com>
2023-03-22 13:53:25 -0400

    made edits to files
    edited some files

--- javascript/takeoutScript.js
+++ javascript/takeoutScript.js
@@ -1,58 +1,33 @@
-function GetCategory(CategoryFileName)
-{
-    var xmlhttp;
-
-    if ( window.XMLHttpRequest )
-    {
+function GetCategory( CategoryFileName ) {
+    let xmlhttp;
+    if ( window.XMLHttpRequest ) {
         xmlhttp = new XMLHttpRequest;
-    }
-    else
-    {
+    } else {
         xmlhttp = new ActiveXObject( "Microsoft.XMLHTTP" );
     }
-
-    xmlhttp.onreadystatechange = function ()
-    {
-        if (( xmlhttp.readyState == 4 ) &&
-            ( xmlhttp.status == 200 ))
-        {
-            document.getElementById( "categoryDisplay" ).innerHTML = xmlhttp.responseText;
-        }
+    xmlhttp.onreadystatechange = () =>  {
+        if ( xmlhttp.readyState != 4 && xmlhttp.status != 200 ) return console.log("Error with the ready state!");
+        document.getElementById( "categoryDisplay" ).innerHTML = xmlhttp.responseText;
     }
-
     xmlhttp.open( "GET", CategoryFileName, true );
-
     xmlhttp.send();
 }
-
-function StartDrag( CurrentEvent )
-{
+function StartDrag( CurrentEvent ) {
     CurrentEvent.dataTransfer.setData( "text/html", CurrentEvent.target.id );
 }
-
-function AllowDrop( CurrentEvent )
-{
+function Drop( CurrentEvent, DestinationID ) {
+    AllowDrop();
+    let DataID = CurrentEvent.dataTransfer.getData( "text/html" );
+    document.getElementById( DestinationID ).appendChild( document.getElementById( DataID ).cloneNode( true ) );
+}
+function AllowDrop( CurrentEvent ) {
     CurrentEvent.preventDefault();
 }
-
-function Drop( CurrentEvent, DestinationID )
-{
-    CurrentEvent.preventDefault();
-
-    var DataID = CurrentEvent.dataTransfer.getData( "text/html" );
-    
-    document.getElementById( DestinationID ).appendChild( document.getElementById( DataID ).cloneNode( true ));
-
-}
-
-function ComputeTotal()
-{
-    var Orders = document.querySelectorAll( "div[id=\"orderlist\"] div" );
-
-    var value = 0;
-    for( var i=0; i<GradeList.length; i++ )
-    {
-        value = Number(value) + Number(Orders[i].getAttribute( "data-point-value" ));
+function ComputeTotal() {
+    let Orders = document.querySelectorAll( "div[id='orderlist'] div" );
+    let value = 0;
+    for( let i=0; i< Orders.length; i++ ) {
+        value += Number( Orders[i].getAttribute( "data-point-value" ) );
     }
-    document.querySelector(".total").innerHTML = '<p class="total">Total: ' + value + '</p>';
+    document.querySelector(".total").innerHTML = `<p class="total">Total: ${value}</p>`;
 }
