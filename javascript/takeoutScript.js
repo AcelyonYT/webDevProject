e743af2d5453d5094cc10bc6c08bb16fd2906cb5
AcelyonYT <95740136+AcelyonYT@users.noreply.github.com>
2023-03-23 22:11:46 -0400

    fixed remove, and drag drop stacking
    remove and stacking works, edited ids so we don't have ids that are the same in the shopping cart

--- javascript/takeoutScript.js
+++ javascript/takeoutScript.js
@@ -6,8 +6,9 @@ function GetCategory( CategoryFileName ) {
         xmlhttp = new ActiveXObject( "Microsoft.XMLHTTP" );
     }
     xmlhttp.onreadystatechange = () =>  {
-        if ( xmlhttp.readyState != 4 && xmlhttp.status != 200 ) return console.log("Error with the ready state!");
-        document.getElementById( "categoryDisplay" ).innerHTML = xmlhttp.responseText;
+        if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
+            document.getElementById( "categoryDisplay" ).innerHTML = xmlhttp.responseText;
+        }
     }
     xmlhttp.open( "GET", CategoryFileName, true );
     xmlhttp.send();
@@ -21,7 +22,36 @@ function AllowDrop( CurrentEvent ) {
 function Drop( CurrentEvent, DestinationID ) {
     CurrentEvent.preventDefault();
     let DataID = CurrentEvent.dataTransfer.getData( "text/html" );
-    document.getElementById( DestinationID ).appendChild( document.getElementById( DataID ).cloneNode( true ) );
+    let movingItem = document.getElementById( DataID );
+    let string = movingItem.innerHTML;
+    let Destination = document.getElementById( DestinationID );
+    let itemExist = document.querySelector(`div[id="${DestinationID}"] div[id="${movingItem.id}"]`);
+    if(itemExist == null){
+        let cloneItem = movingItem.cloneNode( true );
+        cloneItem.setAttribute("data-count", 1);
+        cloneItem.addEventListener( "dblclick", () => { RemoveItem( DestinationID, movingItem.id, string ) } );
+        cloneItem.innerHTML.concat(" x1");
+        Destination.appendChild( cloneItem );
+    }else{
+        let Count = Number( itemExist.getAttribute("data-count") ) + 1;
+        itemExist.setAttribute("data-count", Count);
+        itemExist.innerHTML = `${string} x${Count}`;
+    }
+    ComputeTotal();
+    ComputeTax();
+    ComputeSubtotal();
+}
+function RemoveItem( DestinationID, item, string ){
+    let Result = window.confirm("Do you want to remove this item from your order?");
+    if(!Result) return;
+    let currentItem = document.querySelector(`div[id="${DestinationID}"] div[id="${item}"]`);
+    let Count = Number( currentItem.getAttribute("data-count") ) - 1;
+    if(Count == 0){
+        currentItem.parentNode.removeChild(currentItem);
+    }else{
+        currentItem.setAttribute("data-count", Count);
+        currentItem.innerHTML = `${string} x${Count}`;
+    }
     ComputeTotal();
     ComputeTax();
     ComputeSubtotal();
@@ -29,8 +59,11 @@ function Drop( CurrentEvent, DestinationID ) {
 function ComputeTotal() {
     let Orders = document.querySelectorAll( "div[id='orderlist'] div" );
     let value = 0;
-    for( let i=0; i< Orders.length; i++ ) {
-        value += Number( Orders[i].getAttribute( "data-point-value" ) );
+    for( let i=0; i < Orders.length; i++ ) {
+        let count = Number( Orders[i].getAttribute("data-count") );
+        for(let j = 0; j < count; j++){
+            value += Number( Orders[i].getAttribute( "data-point-value" ) );
+        }
     }
     document.querySelector(".total").innerHTML = `<p class="total">Total: <span id="totalval" data-point-value=${value}>${value}<span></p>`;
 }
