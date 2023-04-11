9068ecc926a2e4a5ffec6fd27d2b9fc14d85fcab
tripp0x0 <127219148+tripp0x0@users.noreply.github.com>
2023-04-11 09:58:22 -0400

    added salads/drinks

--- sql/menuItems.sql
+++ sql/menuItems.sql
@@ -40,3 +40,100 @@ INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
 INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
     VALUES("kids", "Junior Burger", "A plain hamburger or cheese burger with a side of fries served up in a car shaped plate.", 6.99)
 ;
+
+-- Salads
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("salads", "Mystery Meat Salad","A fresh bed of spring mix topped with grilled meat, onions, fried green peppers, 
+                olives, grilled mushrooms, parmesan cheese, and our house-made italian dressing.", 10.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("salads", "Roast Wild Bird Salad","Locally sourced wild bird roasted and plated on a bed of greens topped with grilled onion, 
+                artichoke hearts, parmesan cheese, and house-made basalmic dressing", 11.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("salads", "Roadside Greens Salad","Spring mix greens topped with pomegranate seeds, apple, 
+                feta cheese, walnuts, and house-made honey basalmic dressing.", 9.99)
+;
+
+-- Drinks
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Coke","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Diet Coke","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Sprite","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Root Beer","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Mellow Yellow","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Ginger Ale","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Orange Soda","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Lemonade","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Raspberry Lemonade","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Strawberrry Lemonade","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Sweet Iced Tea","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Unsweet Iced Tea","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Peach Iced Tea","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Raspberry Iced Tea","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Mango Iced Tea","", 1.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Chocolate Milkshake","", 3.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Vanilla Milkshake","", 3.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Strawberry Milkshake","", 3.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "Cookie Monster Milkshake","", 3.99)
+;
+
+INSERT INTO menu( ITEM_CATEGORY, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE )
+    VALUES("drinks", "S'mores Milkshake","", 3.99)
+;
