8b8264eaa5829f85a3b54997c442b9e0e7ab8228
AcelyonYT <95740136+AcelyonYT@users.noreply.github.com>
2023-05-02 10:22:36 -0400

    changed sql
    changed sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,12 +1,11 @@
-create table users
-(
-    USER_EMAIL           varchar(50),
-    USER_PASSWORD        varchar(50),
-    USER_FNAME            varchar(50),
-    USER_LNAME            varchar(50),
-    USER_ADDRESS         varchar(50),
-    USER_CITY            varchar(50),
-    USER_STATE           char(2),
-    USER_ZIP             char(5),
-        primary key( USER_EMAIL )
+-- Active: 1679940753444@@127.0.0.1@3306@homework
+CREATE TABLE users (
+    USER_EMAIL              VARCHAR(60) PRIMARY KEY,
+    USER_PASSWORD           VARCHAR(60),
+    USER_FNAME              VARCHAR(50),
+    USER_LNAME              VARCHAR(50),
+    USER_ADDRESS            VARCHAR(50),
+    USER_CITY               VARCHAR(50),
+    USER_STATE              CHAR(2),
+    USER_ZIP                CHAR(5)
 );
