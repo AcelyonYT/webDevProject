76918a9d324a9487405d0093f88143a19e698f7f
AcelyonYT <95740136+AcelyonYT@users.noreply.github.com>
2023-05-05 13:06:34 -0400

    fixed login
    fixed login

--- php/loginForm.php
+++ php/loginForm.php
@@ -1,4 +1,4 @@
-<form action="index.php" method="POST">
+<form action="login.php" method="POST">
     <p>Email: <input name="Email" type="text" required="true" /></p>
     <p>Password: <input name="Password" type="password" required="true" /></p>
     <p><button type="submit">Login</button></p>
